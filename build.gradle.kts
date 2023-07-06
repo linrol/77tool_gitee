cf581025678f551ac6f955df7808a8496348879a
virit.chen <virit@77hub.com>
2023-07-06 11:13:31 +0800

    <feature-0000>添加kotlin支持，配置阿里maven仓库

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,51 +1,54 @@
 plugins {
-    id("java")
-    id("org.jetbrains.intellij") version "1.5.2"
+  id("java")
+  id("org.jetbrains.kotlin.jvm") version "1.7.20"
+  id("org.jetbrains.intellij") version "1.5.2"
 }
 
 group = "com.linrol.cn"
 version = "1.0.4"
 
 repositories {
-    //TODO change them according every develop requirements
-    maven { url=uri("https://www.jetbrains.com/intellij-repository/releases") }
-    //maven { url=uri("https://www.jetbrains.com/intellij-repository/snapshots") }
-    maven { url=uri("https://cache-redirector.jetbrains.com/intellij-dependencies") }
-    mavenCentral()
+  //TODO change them according every develop requirements
+  maven { url = uri("https://www.jetbrains.com/intellij-repository/releases") }
+  //maven { url=uri("https://www.jetbrains.com/intellij-repository/snapshots") }
+  maven { url = uri("https://cache-redirector.jetbrains.com/intellij-dependencies") }
+  maven {
+    url = uri("https://maven.aliyun.com/repository/public")
+  }
 }
 
 // Configure Gradle IntelliJ Plugin
 // Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
 intellij {
-    version.set("2021.2")
-    type.set("IC") // Target IDE Platform
+  version.set("2021.2")
+  type.set("IC") // Target IDE Platform
 
-    plugins.set(listOf("Git4Idea"))
+  plugins.set(listOf("Git4Idea"))
 }
 
 tasks {
-    // Set the JVM compatibility versions
-    withType<JavaCompile> {
-        sourceCompatibility = "8"
-        targetCompatibility = "8"
-    }
+  // Set the JVM compatibility versions
+  withType<JavaCompile> {
+    sourceCompatibility = "8"
+    targetCompatibility = "8"
+  }
 
-    buildSearchableOptions{
-        enabled = false
-    }
+  buildSearchableOptions {
+    enabled = false
+  }
 
-    patchPluginXml {
-        sinceBuild.set("182")
-        untilBuild.set("231.*")
-    }
+  patchPluginXml {
+    sinceBuild.set("182")
+    untilBuild.set("231.*")
+  }
 
-    signPlugin {
-        certificateChain.set(System.getenv("CERTIFICATE_CHAIN"))
-        privateKey.set(System.getenv("PRIVATE_KEY"))
-        password.set(System.getenv("PRIVATE_KEY_PASSWORD"))
-    }
+  signPlugin {
+    certificateChain.set(System.getenv("CERTIFICATE_CHAIN"))
+    privateKey.set(System.getenv("PRIVATE_KEY"))
+    password.set(System.getenv("PRIVATE_KEY_PASSWORD"))
+  }
 
-    publishPlugin {
-        token.set(System.getenv("PUBLISH_TOKEN"))
-    }
+  publishPlugin {
+    token.set(System.getenv("PUBLISH_TOKEN"))
+  }
 }
