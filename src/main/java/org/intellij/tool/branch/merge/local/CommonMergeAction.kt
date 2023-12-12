116aeba83589344ec0492f048ad760ef308d3681
linrol <linrolmail@gmail.com>
2023-12-12 12:13:36 +0800

    77tool-task-0000-迁移kotlin语法

--- src/main/java/org/intellij/tool/branch/merge/local/CommonMergeAction.kt
+++ src/main/java/org/intellij/tool/branch/merge/local/CommonMergeAction.kt
@@ -1,20 +1,13 @@
-package org.intellij.tool.branch.merge.local;
+package org.intellij.tool.branch.merge.local
 
-import com.intellij.openapi.actionSystem.AnActionEvent;
-import com.intellij.openapi.project.DumbAwareAction;
-import com.intellij.openapi.project.Project;
-import org.jetbrains.annotations.NotNull;
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.project.DumbAwareAction
 
-public class CommonMergeAction extends DumbAwareAction {
-    @Override
-    public void actionPerformed(@NotNull AnActionEvent e) {
-        Project project = e.getProject();
-        if (project == null) {
-            return;
-        }
+class CommonMergeAction : DumbAwareAction() {
+    override fun actionPerformed(e: AnActionEvent) {
+        val project = e.project ?: return
 
-        CommonMergeDialog dialog = new CommonMergeDialog(project, e);
-        dialog.showAndGet();
+        val dialog = CommonMergeDialog(project, e)
+        dialog.showAndGet()
     }
-
 }
--- src/main/java/org/intellij/tool/branch/merge/request/MergeRequestAction.kt
+++ src/main/java/org/intellij/tool/branch/merge/request/MergeRequestAction.kt
@@ -1,66 +1,55 @@
-package org.intellij.tool.branch.merge.request;
+package org.intellij.tool.branch.merge.request
 
-import static com.intellij.openapi.ui.DialogWrapper.OK_EXIT_CODE;
+import com.intellij.dvcs.push.PushInfo
+import com.intellij.dvcs.push.ui.PushActionBase
+import com.intellij.dvcs.push.ui.VcsPushDialog
+import com.intellij.dvcs.push.ui.VcsPushUi
+import com.intellij.openapi.diagnostic.logger
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.ui.DialogWrapper
+import git4idea.repo.GitRepository
+import org.apache.commons.lang3.exception.ExceptionUtils
+import org.intellij.tool.branch.command.GitCommand
+import org.intellij.tool.model.GitCmd
+import org.jetbrains.annotations.Nls
 
-import com.intellij.dvcs.push.PushInfo;
-import com.intellij.dvcs.push.ui.PushActionBase;
-import com.intellij.dvcs.push.ui.VcsPushDialog;
-import com.intellij.dvcs.push.ui.VcsPushUi;
-import com.intellij.openapi.diagnostic.Logger;
-import com.intellij.openapi.project.Project;
-import org.intellij.tool.branch.command.GitCommand;
-import org.intellij.tool.model.GitCmd;
-import git4idea.repo.GitRepository;
-import org.apache.commons.lang3.exception.ExceptionUtils;
-import org.jetbrains.annotations.Nls;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
-
-public class MergeRequestAction extends PushActionBase {
-
-  private static final Logger logger = Logger.getInstance(MergeRequestAction.class);
-
-  MergeRequestAction() {
-    super("Merge Request");
-  }
-
-  @Override
-  protected boolean isEnabled(@NotNull VcsPushUi dialog) {
-    return true;
-  }
-
-  @Override
-  protected @Nls @Nullable String getDescription(@NotNull VcsPushUi dialog, boolean enabled) {
-    return "Create Merge Request";
-  }
-
-  @Override
-  protected void actionPerformed(@NotNull Project project, @NotNull VcsPushUi dialog) {
-    try {
-      GitCmd.clear();
-      dialog.getSelectedPushSpecs().values().stream().flatMap(m -> m.stream().map(PushInfo::getRepository)).forEach(repo -> {
-        // 实现逻辑
-        GitCmd cmd = new GitCmd(project, (GitRepository) repo);
-        GitCommand.push(cmd);
-      });
-      close(dialog, OK_EXIT_CODE);
-    } catch (RuntimeException e) {
-      e.printStackTrace();
-      GitCmd.log(project, ExceptionUtils.getRootCauseMessage(e));
-    }  catch (Throwable e) {
-      close(dialog, OK_EXIT_CODE);
-      e.printStackTrace();
-      GitCmd.log(project, ExceptionUtils.getRootCauseMessage(e));
-      logger.error("GitMergeRequestAction execute failed", e);
+class MergeRequestAction : PushActionBase("Merge Request") {
+    override fun isEnabled(dialog: VcsPushUi): Boolean {
+        return true
     }
 
-  }
-
-  private void close(VcsPushUi dialog, int exitCode) {
-    if (dialog instanceof VcsPushDialog) {
-      // 关闭push窗口
-      VcsPushDialog vcsPushDialog = (VcsPushDialog) dialog;
-      vcsPushDialog.close(exitCode);
+    override fun getDescription(dialog: VcsPushUi, enabled: Boolean): @Nls String {
+        return "Create Merge Request"
     }
-  }
+
+    override fun actionPerformed(project: Project, dialog: VcsPushUi) {
+        try {
+            GitCmd.clear()
+            dialog.selectedPushSpecs.values.flatMap { it.map { obj: PushInfo -> obj.repository } }.forEach {
+                // 实现逻辑
+                val cmd = GitCmd(project, it as GitRepository)
+                GitCommand.push(cmd)
+            }
+            close(dialog, DialogWrapper.OK_EXIT_CODE)
+        } catch (e: RuntimeException) {
+            e.printStackTrace()
+            GitCmd.log(project, ExceptionUtils.getRootCauseMessage(e))
+        } catch (e: Throwable) {
+            close(dialog, DialogWrapper.OK_EXIT_CODE)
+            e.printStackTrace()
+            GitCmd.log(project, ExceptionUtils.getRootCauseMessage(e))
+            logger.error("GitMergeRequestAction execute failed", e)
+        }
+    }
+
+    private fun close(dialog: VcsPushUi, exitCode: Int) {
+        if (dialog is VcsPushDialog) {
+            // 关闭push窗口
+            dialog.close(exitCode)
+        }
+    }
+
+    companion object {
+        private val logger = logger<MergeRequestAction>()
+    }
 }
